48e0b790199f58ef483a0346196a5b5ebb534c4f
Serpentariu13 <serpentarium13@mail.ru>
2024-10-20 03:15:05 +0300

    Remove critters

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,6 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import inline from "@playform/inline";
 import expressiveCode from "astro-expressive-code";
 import pagefind from "astro-pagefind";
 
@@ -62,7 +61,6 @@ export default defineConfig({
     vue(),
     pagefind(),
     preact({ compat: true }),
-    inline({}),
   ],
 
   markdown: {
